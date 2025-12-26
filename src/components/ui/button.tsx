e9ab1064c295af83015124b69e9308d50d9aa513
deveshmishra2003 <devesh.mishra.1904@gmail.com>
2025-12-26 20:53:35 +0530

    button component added

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,13 +1,27 @@
 interface ButtonProps{
-  variant: "primary"|"secondey";
+  variant: "primary"|"secondary";
   size: "sm"| "md" | "lg";
   text: string;
   startIcon?:any;
   endIcon: any;
   onClick: ()=> void;
 }
+const variantStyles={
+  "primary": "bg-purple-600 text-white",
+  "secondary": "bg-purple-300 text-purple-600"
+}
+
+const sizeStyle={
+  "sm": "py-1 px-2",
+  "md": "py-2 px-4",
+  "lg": "py-4 px-6"
+}
+const defaultStyles="rounded-md p-4 flex"
+
+
 export const Button=(props: ButtonProps)=>{
-  return <button></button>
+  return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyle}`}>
+    {props.startIcon ? <div className="pr-2">{props.startIcon}</div>: null} {props.text} {props.endIcon}</button>
 }
 <Button endIcon={"+"} variant="primary" size="md" onClick={()=>{}} text={"assdd"} 
 startIcon={"_"}/>
